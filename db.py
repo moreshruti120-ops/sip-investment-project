7dcb76227f29cb83c65171b1059db893e1a97263
Shruti <moreshruti120@gmail.com>
2026-02-02 23:48:05 +0530

    Clean repository

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="localhost",
-        user="sipuser",
-        password="sip123",
-        database="sip_db"
-    )
